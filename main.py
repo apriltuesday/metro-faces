037efb4f5e147391422f7797028fe034ec16cd42
April Shen <april@cs.washington.edu>
2014-04-02 11:05:17 -0700

    added back time & place features

--- main.py
+++ main.py
@@ -19,46 +19,34 @@ import Queue
 NUM_LINES = 4
 NUM_CLUSTERS = 80 #???
 
-def coverage(map, xs):
+def coverage(map, xs, weights):
     """
     Computes coverage of the set of chains map.  See
     metromaps for details.
     """
     subset = list(set(cbook.flatten(map)))
-    # Compute coverages, vector-style
-#    vects = np.hstack([faces, times, places]) #XXX regions
-#    weights = np.hstack([faceWeights, timeWeights, placeWeights])
-    weights = np.ones(xs.shape[1])
-    total = (1 - np.prod(1 - xs[subset], axis=0)).dot(weights) # TODO add back weighting
+    total = (1 - np.prod(1 - xs[subset], axis=0)).dot(weights)
     return total
 
 
-def greedy(map, candidates, xs):
+def greedy(map, candidates, xs, weights):
     """
     Greedily choose the max-coverage candidate and add to map.
     """
     maxCoverage = 0.0
     maxP = 0
     for p in candidates:
-        c = coverage(map + [p], xs)
+        c = coverage(map + [p], xs, weights)
         if c > maxCoverage:
             maxCoverage = c
             maxP = p
     map.append(maxP)
 
 
-if __name__ == '__main__':
-#    args = sys.argv
-
-    # Load data
-    mat = io.loadmat('../data/April_full_new.mat')
-    images = mat['images'][:,0]
-    faces = mat['faces'] + mat['facesBinary'] 
-#    years = mat['timestamps'].reshape(n)
-#    longitudes = mat['longitudes'].reshape(n)
-#    latitudes = mat['latitudes'].reshape(n)
-
-    # Get a master list of names from contacts.xml
+def getNames():
+    """
+    Get a master list of names from contacts.xml
+    """
     names = []
     for line in open('../data/faces/contacts.xml'):
         try:
@@ -67,28 +55,90 @@ if __name__ == '__main__':
             names.append(name)
         except ValueError:
             continue
-    names = np.array(names)
+    return np.array(names)
+
+
+def bin(values, k):
+    """
+    Bin values into k bins and returns binary vector indicating bin
+    membership. Assumes we can sort.  May be infs but no nans.
+    """
+    n = values.shape[0]
+    items = np.arange(n)
+
+    # sorted items that have a valid value
+    sortedItems = sorted(items[np.isfinite(values)], key=lambda i: values[i])
+    # unique values, sorted
+    sortedVals = sorted(set(values))
+    # number of values per bin
+    num = int(np.ceil(len(sortedVals) / k))
+    bins = [sortedVals[x:x+num] for x in range(0, len(sortedVals), num)]
+
+    # Compute binary vector for each item based on its value
+    vectors = np.zeros((n, len(bins)))
+    for i in items:
+        if np.isfinite(values[i]):
+            whichBin = map(lambda x: values[i] in x, bins).index(True)
+            vectors[i, whichBin] = 1
+    return vectors
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) < 2:
+        k = 10
+    else:
+        k = int(args[1]) # number of year bins to select; ultimately a function of zooming/the UI
+
+    # Load data
+    mat = io.loadmat('../data/April_full_gps.mat')
+    images = mat['images'][:,0]
+    faces = mat['faces'] + mat['facesBinary'] 
+    years = mat['timestamps'].flatten()
+    longitudes = mat['longitudes'].flatten()
+    latitudes = mat['latitudes'].flatten()
+    names = getNames()
+    print 'done loading'
 
     # Omit people who don't appear
     names = names[~np.all(faces == 0, axis=0)]
     faces = faces[:, ~np.all(faces == 0, axis=0)]
-
     n, m = faces.shape
     photos = np.arange(n)
     ppl = np.arange(m)
-    print 'done loading'
+
+    # Bin times and GPS coordinates. If value is missing we assume it covers nothing.
+    times = bin(years, k)
+    times = times[:, ~np.all(times == 0, axis=0)]
+    longs = bin(longitudes, k)
+    lats = bin(latitudes, k)
+    # just need place-bins
+    places = np.zeros((n, k**2))
+    nonLongs = np.nonzero(longs)
+    nonLats = np.nonzero(lats)
+    for img, lo, la in zip(nonLongs[0], nonLongs[1], nonLats[1]):
+        places[img, lo + la*k] = 1
+    places = places[:, ~np.all(places == 0, axis=0)]
+
+    # Weight importance of faces, times, places by frequency... normalize
+    faceWeights = np.apply_along_axis(np.count_nonzero, 0, faces)
+    faceWeights = faceWeights / np.linalg.norm(faceWeights)
+    timeWeights = np.apply_along_axis(np.count_nonzero, 0, times)
+    timeWeights = timeWeights / np.linalg.norm(timeWeights)
+    placeWeights = np.apply_along_axis(np.count_nonzero, 0, places)
+    placeWeights = placeWeights / np.linalg.norm(placeWeights)
 
     # Form adjacency matrix of the social graph
     A = np.array([np.sum(np.product(faces[:,[i, j]], axis=1)) for i in ppl for j in ppl]).reshape((m,m))
 
     # Graph that sucker
-#    colors = cm.get_cmap(name='Spectral')
     G = nx.Graph(A)
     nodes = [A[i,i] for i in ppl]
     edges = []
     for (u,v) in G.edges():
         edges.append(A[u,v])
 
+    #colors = cm.get_cmap(name='Spectral')
     #nx.draw(G, node_color=nodes, edge_color=edges, cmap=colors, edge_cmap=colors)
     layout = nx.spring_layout(G)
     nx.draw_networkx_nodes(G, pos=layout, node_size=[x*2 for x in nodes])
@@ -104,7 +154,7 @@ if __name__ == '__main__':
     # Choose clusters of faces to use for lines
     whichClusters = []
     for i in range(NUM_LINES):
-        greedy(whichClusters, clusters, faces.T)
+        greedy(whichClusters, clusters, faces.T, np.ones(n))
     for i in range(NUM_LINES):
         for j in whichClusters[i]:
             print j, names[j]
@@ -112,10 +162,12 @@ if __name__ == '__main__':
 
     # For each face cluster, choose representative photos
     map = []
+    vects = np.hstack([faces, times, places])
+    weights = np.hstack([faceWeights, timeWeights, placeWeights])
     for cl in whichClusters:
         # get photos that contain faces in this cluster
         pool = set(np.nonzero(faces[:,cl])[0])
-        greedy(map, pool, faces)
+        greedy(map, pool, vects, weights)
         # TODO want to cover as many faces in this cluster as possible
 
     # Display
